10ec73362d2cf8f244bc4c6a72528815f9796c71
Shiyah <shiyah@theidio.tech>
2018-05-15 13:04:03 +0100

    init

---
+++ src/Lib.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE TypeFamilies               #-}
+module Lib where
